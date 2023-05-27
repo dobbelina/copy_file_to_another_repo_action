14f56e3a076432f9be78e3a7d79879c8a36fcb60
dobbelina <dobbelina2000@yahoo.com>
2023-05-27 02:57:47 +0200

    Trailing slash cleanup

--- entrypoint.sh
+++ entrypoint.sh
@@ -51,8 +51,8 @@ for SOURCE_FILE in $SOURCE_FILES; do
   SOURCE_FILE=$(echo "$SOURCE_FILE" | xargs)
   
   # Handle source file
-  if [ -d "$SOURCE_FILE" ]; then
-    rsync -avrh --exclude "$INPUT_EXCLUDE_FILES" "$SOURCE_FILE"/* "$DEST_COPY"
+  if [ -d "$SOURCE_FILE" ] && [ "${SOURCE_FILE: -1}" != "/" ]; then
+    rsync -avrh --exclude "$INPUT_EXCLUDE_FILES" "$SOURCE_FILE"/ "$DEST_COPY"
   else
     rsync -avrh --exclude "$INPUT_EXCLUDE_FILES" "$SOURCE_FILE" "$DEST_COPY"
   fi
